5e577c70b333fc119a096ddac249f01c5efa2e9a
Andrey Medvedev <medveddevaa@icloud.com>
2023-02-19 15:39:13 +0300

    feat(web): setup elonkit, react-router

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,19 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { ruRU } from "@mui/material/locale";
 
-import "./index.css";
+import { App } from "./App";
 
+import { createTheme, palettes, ru, ThemeProvider } from "@elonkit/react";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const theme = createTheme(
+  {
+    palette: {
+      ...palettes.common,
+      ...palettes.light
+    }
+  },
+  {
+    ...ruRU,
+    ru
+  }
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
